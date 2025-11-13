4236dc1b2f790f0b88112f45cd1dfcb15741d918
regawan <regawan@regawan-S540-ubt-24.04>
2025-11-13 14:02:46 +0800

    Added todos

--- graphrag.py
+++ graphrag.py
@@ -1,17 +1,19 @@
 import neo4j
+# TODO: Update model imports if change
 from neo4j_graphrag.llm import OpenAILLM as LLM
 from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings as Embeddings
 from neo4j_graphrag.experimental.pipeline.kg_builder import SimpleKGPipeline
 from neo4j_graphrag.retrievers import VectorRetriever
 from neo4j_graphrag.generation.graphrag import GraphRAG
 
-NEO4J_URI = ""
+NEO4J_URI = "neo4j+s://cb673ac6.databases.neo4j.io"
 NEO4J_USERNAME = "neo4j"
 NEO4J_PASSWORD = "lDHDAAFTkU91TDH0echHMuVCch6KAgeBCqG0xdBsD-A"
 
 neo4j_driver = neo4j.GraphDatabase.driver(NEO4J_URI,
                                           auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
 
+# TODO: Decide on model to use
 ex_llm=LLM(
    model_name="gpt-4o-mini",
    model_params={
@@ -22,15 +24,16 @@ ex_llm=LLM(
 embedder = Embeddings()
 
 # 1. Build KG and Store in Neo4j Database
-kg_builder_pdf = SimpleKGPipeline(
+kg_builder = SimpleKGPipeline(
    llm=ex_llm,
    driver=neo4j_driver,
    embedder=embedder,
-   from_pdf=True
+   from_pdf=False
 )
-await kg_builder_pdf.run_async(file_path='precision-med-for-lupus.pdf')
+await kg_builder.run_async(file_path='')
 
 # 2. KG Retriever
+# TODO: Implement vector cypher retriever, now it is a simple vector retriever
 vector_retriever = VectorRetriever(
    neo4j_driver,
    index_name="text_embeddings",
@@ -44,6 +47,3 @@ rag = GraphRAG(llm=llm, retriever=vector_retriever)
 # 4. Run
 response = rag.search( "How is precision medicine applied to Lupus?")
 print(response.answer)
-
-#output
-Precision medicine in systemic lupus erythematosus (SLE) is an evolving approach that aims to tailor treatment based on individual genetic, epigenetic, and pathophysiological characteristics…
