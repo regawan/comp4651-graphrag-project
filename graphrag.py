1fbad55f9b4776248971623410e5adb5204493cf
regawan <regawan@regawan-S540-ubt-24.04>
2025-11-13 23:53:11 +0800

    Model change

--- graphrag.py
+++ graphrag.py
@@ -1,9 +1,9 @@
 import neo4j
-# TODO: Update model imports if change
-from neo4j_graphrag.llm import OpenAILLM as LLM
-from neo4j_graphrag.embeddings.openai import OpenAIEmbeddings as Embeddings
+from neo4j_graphrag.llm import VertexAILLM
+from vertexai.generative_models import GenerationConfig
+from neo4j_graphrag.embeddings import VertexAIEmbeddings
 from neo4j_graphrag.experimental.pipeline.kg_builder import SimpleKGPipeline
-from neo4j_graphrag.retrievers import VectorRetriever
+from neo4j_graphrag.retrievers import VectorCypherRetriever
 from neo4j_graphrag.generation.graphrag import GraphRAG
 
 NEO4J_URI = "neo4j+s://cb673ac6.databases.neo4j.io"
@@ -13,15 +13,14 @@ NEO4J_PASSWORD = "lDHDAAFTkU91TDH0echHMuVCch6KAgeBCqG0xdBsD-A"
 neo4j_driver = neo4j.GraphDatabase.driver(NEO4J_URI,
                                           auth=(NEO4J_USERNAME, NEO4J_PASSWORD))
 
-# TODO: Decide on model to use
-ex_llm=LLM(
-   model_name="gpt-4o-mini",
-   model_params={
-       "response_format": {"type": "json_object"},
-       "temperature": 0
-   })
+generation_config = GenerationConfig(temperature=0.0)
 
-embedder = Embeddings()
+ex_llm= VertexAILLM(
+   model_name="gemini-2.5-flash",
+   generation_config=generation_config
+)
+
+embedder = VertexAIEmbeddings(model='text-embedding-004', rate_limit_handler=None)
 
 # 1. Build KG and Store in Neo4j Database
 kg_builder = SimpleKGPipeline(
@@ -33,16 +32,31 @@ kg_builder = SimpleKGPipeline(
 await kg_builder.run_async(file_path='')
 
 # 2. KG Retriever
-# TODO: Implement vector cypher retriever, now it is a simple vector retriever
-vector_retriever = VectorRetriever(
-   neo4j_driver,
+vc_retriever = VectorCypherRetriever(
+   driver,
    index_name="text_embeddings",
-   embedder=embedder
+   embedder=embedder,
+   retrieval_query="""
+//1) Go out 2-3 hops in the entity graph and get relationships
+WITH node AS chunk
+MATCH (chunk)<-[:FROM_CHUNK]-()-[relList:!FROM_CHUNK]-{1,2}()
+UNWIND relList AS rel
+
+//2) collect relationships and text chunks
+WITH collect(DISTINCT chunk) AS chunks,
+ collect(DISTINCT rel) AS rels
+
+//3) format and return context
+RETURN '=== text ===n' + apoc.text.join([c in chunks | c.text], 'n---n') + 'nn=== kg_rels ===n' +
+ apoc.text.join([r in rels | startNode(r).name + ' - ' + type(r) + '(' + coalesce(r.details, '') + ')' +  ' -> ' + endNode(r).name ], 'n---n') AS info
+"""
 )
 
 # 3. GraphRAG Class
-llm = LLM(model_name="gpt-4o")
-rag = GraphRAG(llm=llm, retriever=vector_retriever)
+llm = VertexAILLM(
+    model_name="gemini-2.5-flash", generation_config=generation_config
+)
+rag = GraphRAG(llm=llm, retriever=vc_retriever)
 
 # 4. Run
 response = rag.search( "How is precision medicine applied to Lupus?")
